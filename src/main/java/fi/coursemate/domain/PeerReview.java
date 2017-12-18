ea6109f18074b2da07f8242c27d71424b14efe25
juhahinkula <hinkula@gmail.com>
2017-12-18 14:17:02 +0200

    review functionality

--- src/main/java/fi/coursemate/domain/PeerReview.java
+++ src/main/java/fi/coursemate/domain/PeerReview.java
@@ -1,13 +1,21 @@
 package fi.coursemate.domain;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class PeerReview {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)	
@@ -20,6 +28,12 @@ public class PeerReview {
     private long courseid;
     private int grade;
     private String description;
+	
+	@CreatedDate
+	private Date createdDate;	
+
+	@CreatedBy
+	private String createdBy;
 	
     public PeerReview() {}
     
@@ -67,5 +81,21 @@ public class PeerReview {
 	
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
 	}
 }
