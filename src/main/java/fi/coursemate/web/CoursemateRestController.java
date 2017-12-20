6ff54dd461a537148e06c89b2cb8483e8c27268c
juhahinkula <hinkula@gmail.com>
2017-12-20 21:36:50 +0200

    new gueries

--- src/main/java/fi/coursemate/web/CoursemateRestController.java
+++ src/main/java/fi/coursemate/web/CoursemateRestController.java
@@ -7,12 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
