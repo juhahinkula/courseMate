bbef969893a16afcb1532a7fb01764ca68a5fdcb
juhahinkula <hinkula@gmail.com>
2017-12-22 15:49:47 +0200

    exports added to review

--- src/main/java/fi/coursemate/web/UserController.java
+++ src/main/java/fi/coursemate/web/UserController.java
@@ -1,18 +1,21 @@
 package fi.coursemate.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
 
 import fi.coursemate.domain.SignupForm;
+import fi.coursemate.domain.SignupTeacherForm;
 import fi.coursemate.domain.Student;
 import fi.coursemate.domain.StudentRepository;
 import fi.coursemate.domain.User;
@@ -31,10 +34,16 @@ public class UserController {
     	model.addAttribute("signupform", new SignupForm());
         return "signup";
     }	
+
+	@PreAuthorize("hasAuthority('ADMIN')")    
+    @RequestMapping(value = "signupteacher")
+    public String addTeacher(Model model){
+    	model.addAttribute("signupform", new SignupForm());
+        return "signupTeacher";
+    }	   
     
     @RequestMapping(value = "saveuser", method = RequestMethod.POST)
     public String save(@Valid @ModelAttribute("signupform") SignupForm signupForm, BindingResult bindingResult) {
-    	System.out.println(bindingResult.toString());
     	if (!bindingResult.hasErrors()) { // validation errors
     		if (signupForm.getPassword().equals(signupForm.getPasswordCheck())) { // check password match		
 	    		String pwd = signupForm.getPassword();
@@ -66,7 +75,38 @@ public class UserController {
     	else {
     		return "signup";
     	}
-    	return "redirect:/login";    	
+    	return "redirect:/login?signup=true";    	
     }    
-    
+
+    @RequestMapping(value = "saveteacher", method = RequestMethod.POST)
+    public String saveteacher(@Valid @ModelAttribute("signupform") SignupTeacherForm signupForm, BindingResult bindingResult) {
+    	if (!bindingResult.hasErrors()) { // validation errors
+    		if (signupForm.getPassword().equals(signupForm.getPasswordCheck())) { // check password match		
+	    		String pwd = signupForm.getPassword();
+		    	BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
+		    	String hashPwd = bc.encode(pwd);
+	
+		    	User newUser = new User();
+		    	newUser.setPasswordHash(hashPwd);
+		    	newUser.setUsername(signupForm.getUsername());
+		    	newUser.setRole("ADMIN");
+		    			    	
+		    	if (repository.findByUsername(signupForm.getUsername()) == null) {
+		    		repository.save(newUser);
+		    	}
+		    	else {
+	    			bindingResult.rejectValue("username", "error.userexists", "Username already exists");    	
+	    			return "signup";		    		
+		    	}
+    		}
+    		else {
+    			bindingResult.rejectValue("passwordCheck", "error.pwdmatch", "Passwords does not match");    	
+    			return "signup";
+    		}
+    	}
+    	else {
+    		return "signup";
+    	}
+    	return "redirect:/login?signup=true";    	
+    }        
 }
