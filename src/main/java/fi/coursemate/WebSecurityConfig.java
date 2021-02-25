0ef7f069aebf235e8b12e05e3c8725110da8ed64
juhahinkula <hinkula@gmail.com>
2021-02-25 16:06:57 +0200

    login bug fixed

--- src/main/java/fi/coursemate/WebSecurityConfig.java
+++ src/main/java/fi/coursemate/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/courses")
+                .defaultSuccessUrl("/courses", true)
                 .permitAll()
             	.and()
             .logout()
